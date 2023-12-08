cf363c8db5d389079ed2749c20130f7331427bf5
Frederico Ferreira <frederico.ferreira@grama.io>
2023-12-08 00:22:43 +0000

    Adding changes

---
+++ src/blackhole.h
@@ -0,0 +1,123 @@
+#ifndef BLACKHOLE_H
+#define BLACKHOLE_H
+
+#include "ofMain.h"
+#include "extras.h"
+#include "drawing_extras.h"
+
+class Blackhole {
+    public:
+        ofVec3f pos;
+        GLdouble radius;
+        int count = 0;
+        int mesh;
+        long unsigned mass;
+
+        Blackhole(){
+            pos = ofVec3f(0, 0, 0);
+            radius = 0;
+            mesh = 50;
+            mass = 60000;
+        }
+
+        Blackhole(GLfloat x_, GLfloat y_, GLfloat z_, GLfloat radius_, int mesh_, long unsigned mass_) {
+            pos = ofVec3f(x_, y_, z_);
+            radius = radius_;
+            mesh = mesh_;
+            mass = mass_;
+        }
+
+        void draw() { //TODO posicionamento especial????
+            if(radius < 300 and radius > 0.1){
+                radius += 1/60.0;
+            }
+            count += 1;
+            glColor3f(1, 1, 1);
+            glTranslatef(pos.x, pos.y, pos.z);
+            glScalef(radius,radius,radius);
+            glRotated(count,0,0,1);
+            unit_sphere();
+        }
+    
+    private:
+        void unit_sphere() {
+            GLfloat theta_s, theta_e;
+            GLfloat theta_step = 2.0 * M_PI / GLfloat(mesh);
+            GLfloat phi_s, phi_e;
+            GLfloat phi_step = M_PI / GLfloat(mesh);
+            GLfloat x1, y1, z1;
+            GLfloat x2, y2, z2;
+            GLfloat x3, y3, z3;
+            GLfloat x4, y4, z4;
+
+            for (int i = 0; i < mesh; i++) {
+                theta_s = i * theta_step;
+                theta_e = theta_s + theta_step;
+                glBegin(GL_TRIANGLES);
+                phi_s = M_PI /2;
+                phi_e = M_PI /2 - phi_step;
+                x1 = sin(theta_s) * cos(phi_s);
+                y1 = cos(theta_s) * cos(phi_s);
+                z1 = sin(phi_s);
+                x2 = sin(theta_e) * cos(phi_e);
+                y2 = cos(theta_e) * cos(phi_e);
+                z2 = sin(phi_e);
+                x3 = sin(theta_s) * cos(phi_e);
+                y3 = cos(theta_s) * cos(phi_e);
+                z3 = sin(phi_e);
+                glVertex3d(x3, y3, z3);
+                glVertex3d(x2, y2, z2);
+                glVertex3d(x1, y1, z1);
+                glEnd();
+                glBegin(GL_QUADS);
+                for (int j = 1; j < mesh-1; j++) {
+                    if(i == j){
+                        glColor3f(1, 0, 0);
+                    }else{
+                        glColor3f(1, 1, 1);
+                    }
+                    phi_s = M_PI /2 - j * phi_step;
+                    phi_e = M_PI /2 - (j+1) * phi_step;
+                    x1 = sin(theta_s) * cos(phi_s);
+                    y1 = cos(theta_s) * cos(phi_s);
+                    z1 = sin(phi_s);
+                    x2 = sin(theta_e) * cos(phi_s);
+                    y2 = cos(theta_e) * cos(phi_s);
+                    z2 = sin(phi_s);
+                    x3 = sin(theta_e) * cos(phi_e);
+                    y3 = cos(theta_e) * cos(phi_e);
+                    z3 = sin(phi_e);
+                    x4 = sin(theta_s) * cos(phi_e);
+                    y4 = cos(theta_s) * cos(phi_e);
+                    z4 = sin(phi_e);
+                    glVertex3f(x4, y4, z4);
+                    glVertex3f(x3, y3, z3);
+                    glVertex3f(x2, y2, z2);
+                    glVertex3f(x1, y1, z1);
+                    // glNormal3f(x1, y1, z1);
+                    // glNormal3f(x2, y2, z2);
+                    // glNormal3f(x3, y3, z3);
+                    // glNormal3f(x4, y4, z4);
+                }
+                glEnd();
+                glBegin(GL_TRIANGLES);
+                phi_s = M_PI /2 - (mesh-1) * phi_step;
+                phi_e = - M_PI /2;
+                x1 = sin(theta_s) * cos(phi_s);
+                y1 = cos(theta_s) * cos(phi_s);
+                z1 = sin(phi_s);
+                x2 = sin(theta_e) * cos(phi_s);
+                y2 = cos(theta_e) * cos(phi_s);
+                z2 = sin(phi_s);
+                x3 = sin(theta_e) * cos(phi_e);
+                y3 = cos(theta_e) * cos(phi_e);
+                z3 = sin(phi_e);
+                glVertex3d(x3, y3, z3);
+                glVertex3d(x2, y2, z2);
+                glVertex3d(x1, y1, z1);
+                glEnd();
+            }
+        }
+};
+
+#endif
---
+++ src/ofApp.cpp
@@ -0,0 +1,267 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	glEnable(GL_DEPTH_TEST);
+	ofBackground(0.05, 0.05, 0.05);
+	glLineWidth(2);
+    ofSetFrameRate(60);
+
+    maxFPS = 60;
+    maxPeriod = 1 / maxFPS;
+    lastTime = ofGetElapsedTimef();
+
+	//view
+	view = 0;
+    //variable for the lens
+    lens = true;
+
+	//floor parameters
+	resX = 30;
+	resY = 20;
+	floorWidth = gw() * 0.6;
+	floorHeight = gw() * 0.45;
+	floorHeightPos = 0.;
+
+    //building parameters
+    baseWidth = floorWidth / GLfloat(resX);
+    baseHeight = floorHeight / GLfloat(resY);
+    baseWidthMin = floorWidth / GLfloat(resX) * 0.3;
+	baseWidthMax = floorWidth / GLfloat(resX) * 0.9;
+    baseHeightMin = floorHeight / GLfloat(resY) * 0.3;
+	baseHeightMax = floorHeight / GLfloat(resY) * 0.9;
+	baseDepthMax = 200;
+    baseDepthMin = 10;
+	basePos.x = 0;
+	basePos.y = 0;
+	basePos.z = 0;
+
+    //mesh
+    mesh = 5;
+    long unsigned mass =  6 * pow(10, 13);
+    blackhole = {0, 0, baseDepthMax+200, 0, 50, mass};
+
+    baseGrid.clear();
+    for(int i = 0; i < resX; i++){
+        baseGrid.push_back({});
+        for(int j = 0; j < resY; j++){
+            baseGrid[i].push_back({ofRandom(baseWidthMin, baseWidthMax), 
+            ofRandom(baseHeightMin, baseHeightMax), ofRandom(baseDepthMin, baseDepthMax),
+            ofRandom(0,1), ofRandom(0,1), ofRandom(0,1), 
+            i* baseWidth + baseWidth * 0.5 - floorWidth * 0.5,
+            j * baseHeight + baseHeight * 0.5 - floorHeight * 0.5, 0, mesh, blackhole});
+        }
+    }
+
+    angle = 0;
+    inc_angle = 0;
+    lookDistance = 700;
+    dis_z = 500;
+}
+ 
+//--------------------------------------------------------------
+void ofApp::update(){
+      for(int i = 0; i < resX; i++){
+            for(int j = 0; j < resY; j++){
+                baseGrid[i][j].update();
+            }
+        }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+    int lensAngle = 60;
+    GLfloat alpha = 10;
+    GLfloat beta = 1000;
+
+    //lens and perspective
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if(lens){
+        perspective(lensAngle, alpha, beta);
+    }
+    else{
+        glOrtho(-gw() * 0.5, gw() * 0.5, -gh() * 0.5, gh() * 0.5, -1000, 10000);
+    }
+
+    //Camera positions
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glPushMatrix();//master push
+    switch (view) {
+    case 0:
+        lookat(0,0,lookDistance, 0,0,0, 0,1,0);
+        break;
+    case 1:
+        dis_z += inc_z;
+        angle += inc_angle;
+        lookat(lookDistance*sin(angle),-lookDistance*cos(angle), dis_z, 0,0,0, 0,0,1);
+        break;
+    case 2:
+        dis_z += inc_z;
+        angle += (2*PI/360)/2;
+        lookat(lookDistance*sin(angle),-lookDistance*cos(angle), 500, 0,0,0, 0,0,1);
+        break;
+    }
+
+
+    //floor
+    glPushMatrix();//floor push
+        glScalef(floorWidth, floorHeight, 1.);
+        glColor3f(0.5, 0.5, 0.5);
+        malha_unit(resX, resY);
+    glPopMatrix();//floor pop
+
+    //skyscrapers
+    glPushMatrix();//skyscrapers master push
+        for(int i = 0; i < resX; i++){
+            for(int j = 0; j < resY; j++){
+                baseGrid[i][j].draw();
+                baseGrid[i][j].blackhole = blackhole;
+            }
+        }
+    glPopMatrix();//skyscrapers master poop
+
+    glPushMatrix();
+        blackhole.draw();
+    glPopMatrix();
+    glPopMatrix();//master pop
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	switch (key) {
+	case '1':
+		glDisable(GL_CULL_FACE);
+		break;
+	case '2':
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT);
+		break;
+	case '3':
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_BACK);
+		break;
+	case '4':
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT_AND_BACK);
+		break;
+	case 'f':
+        if(grid){
+		    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            grid = false;
+        }else{
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            grid = true;
+        }
+
+		break;
+	case 'g':
+		view ++;
+        if (view > 2) {
+            view = 0;
+        }
+		break;
+    case 'v':
+		lens = !lens; 
+		break;
+	case OF_KEY_LEFT:
+		inc_angle = - (2*PI/360)/2;
+		break;
+	case OF_KEY_RIGHT:
+		inc_angle = (2*PI/360)/2;
+		break;
+	case OF_KEY_UP:
+		inc_z = 2;
+		break;
+	case OF_KEY_DOWN:
+		inc_z = -2;
+		break;
+    case '+':
+        lookDistance-=5;
+        break;
+    case '-':
+        lookDistance+=5;
+        break;
+    case ' ':
+        blackhole.radius = 1;
+        for(int i = 0; i < resX; i++){
+            for(int j = 0; j < resY; j++){
+                baseGrid[i][j].decrement = 1;
+                if(baseGrid[i][j].deleted){
+                    baseGrid[i][j] = {ofRandom(baseWidthMin, baseWidthMax), 
+                    ofRandom(baseHeightMin, baseHeightMax), ofRandom(baseDepthMin, baseDepthMax),
+                    ofRandom(0,1), ofRandom(0,1), ofRandom(0,1), 
+                    i* baseWidth + baseWidth * 0.5 - floorWidth * 0.5,
+                    j * baseHeight + baseHeight * 0.5 - floorHeight * 0.5, 0, mesh, blackhole};
+                }
+            }
+        }
+        break;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    switch (key) {
+    case OF_KEY_UP:
+		inc_z = 0;
+		break;
+	case OF_KEY_DOWN:
+		inc_z = 0;
+		break;
+    case OF_KEY_LEFT:
+		inc_angle = 0;
+		break;
+	case OF_KEY_RIGHT:
+		inc_angle = 0;
+		break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+	setup();
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
